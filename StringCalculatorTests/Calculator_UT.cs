43bb8888799215ac668ab2535e8352176cec6440
Darren Hale <darrenhale@Darrens-MacBook-Pro.local>
2018-03-13 14:23:44 -0500

    Added the rest of the test cases I wanted.

--- StringCalculatorTests/Calculator_UT.cs
+++ StringCalculatorTests/Calculator_UT.cs
@@ -14,12 +14,45 @@ namespace StringCalculatorTests
             Assert.Equal(0, result);
         }
 
-	[Fact]
-	public void AddSingleNumberReturnsItself()
-	{
-	    Calculator testObject = new Calculator();
-	    var result = testObject.Add("1");
-	    Assert.Equal(1, result);
-	}
+    	[Fact]
+	    public void AddSingleNumberReturnsItself()
+	    {
+	        Calculator testObject = new Calculator();
+	        var result = testObject.Add("1");
+	        Assert.Equal(1, result);
+	    }
+
+        [Fact]
+        public void AddTwoNumbersSeparatedByComma()
+        {
+            Calculator testObject = new Calculator();
+            var result = testObject.Add("2,3");
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void AddMoreThanTwoNumbersSeparatedByComma()
+        {
+            Calculator testObject = new Calculator();
+            var result = testObject.Add("1,2,3,4");
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void CanSpecifyTheListDelimiter()
+        {
+            Calculator testObject = new Calculator();
+            testObject.SetDelimiter('|');
+            var result = testObject.Add("1|2|3|4");
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void AddThrowsExceptionWithUnrecognizableInput()
+        {
+            Calculator testObject = new Calculator();
+            Exception ex = Assert.Throws<Exception>(() => testObject.Add("abcd"));
+            Assert.Equal("Invalid input values.", ex.Message);
+        }
     }
 }
