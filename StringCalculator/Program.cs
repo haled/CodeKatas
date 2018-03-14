dd48ee6671fffc314faf6f75e33678f363dfe65f
Darren Hale <darrenhale@Darrens-MacBook-Pro.local>
2018-03-14 17:05:36 -0500

    I felt bad not having a functioning program. Fixed that.

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -6,7 +6,11 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a list of numbers separated by a comma (,).");
+            var numbers = Console.ReadLine();
+            var calc = new Calculator();
+            var result = calc.Add(numbers);
+            Console.WriteLine("The sum of the entered numbers is {0}.", result);
         }
     }
 }
